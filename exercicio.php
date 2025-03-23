527bb95c9ce32b97ace9f29c01786926687d93bd
Muacidevjava <muaci@yahoo.com>
2025-03-23 08:39:41 -0300

    visualizção do exercicio

---
+++ exercicio.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="assets/css/estilo.css" />
+    <title>Exercício PHP</title>
+  </head>
+  <body>
+      <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+      </header>
+      <main class=" principal">
+        <div class="conteudo">
+          
+        </div>
+      </main>
+      <footer class="rodape">
+        MUACI &copy; <?= date('Y'); ?>
+      </footer>
+
+
+
+  </body>
+</html>
